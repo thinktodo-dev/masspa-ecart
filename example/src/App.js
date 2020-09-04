62a62d49dc8d28a0ea067338d72dea84e82a26f7
ngahuynh <nga181096@gmail.com>
2020-09-04 16:44:03 +0700

    caculate tempPrice

--- example/src/App.js
+++ example/src/App.js
@@ -13,14 +13,15 @@ export default class ECartComponent extends React.Component {
     super(props);
     this.state = {
       quantity: 0,
-      tempPrice: 0,
-      finalPrice: 0,
+      // tempPrice: 0,
+      // finalPrice: 0,
       data : [
         {
           id: 1,
           name: "Nước Giặt Ariel Đậm Đặc Dạng Túi 3.6kg",
           provider: "Kim Spa",
           price: 200000,
+          sale_price: 100000,
           quantity: 1
         },
         {
@@ -28,22 +29,47 @@ export default class ECartComponent extends React.Component {
          name: "Điện Thoại iPhone 7 Plus 128GB - Chính Hãng Mã VN/A - Black",
           provider: "Kim Spa",
           price: 300000,
+          sale_price: 100000,
           quantity: 1
          },
          {
           id: 3,
           name: "Điện Thoại iPhone 6 Plus 128GB - Chính Hãng Mã VN/A - Black",
            provider: "Kim Spa",
-           price: 300000,
+          sale_price: 100000,
+          price: 300000,
            quantity: 23
           }
-        ]
+        ],
+        text : {
+          shopping_cart: "GIỎ HÀNG",
+          provider: "Cung cấp bởi",
+          delete: "Xoá",
+          delete: "Xoá",
+          buy_later: "Mua sau",
+          temp_price: "Tạm tính",
+          total_price: "Thành tiền",
+          proceed_to_order: "Tiến hành đặt hàng",
+          empty_cart: "Không có sản phẩm nào trong giỏ hàng của bạn",
+          product: "Sản phẩm",
+          includedVAT: "Đã bao gồm VAT nếu có"
+        }
     }
   }
+
   submitOrder = (data) => {
      console.log(data)
   }
   render() {
-    return (<ExampleComponent customStyleContainerCart = {{backgroundColor: "#f9f9f9"}} customStyleContainerOrder = {{padding: "20px", backgroundColor: "#f9f9f9"}} data = {this.state.data} tempPrice = {100000} finalPrice= {300000} changeInputQuantity = {this.changeInputQuantity} submitOrder = {this.submitOrder} />)
+    console.log(this.state.text)
+    return (<ExampleComponent 
+      customStyleContainerCart = {{backgroundColor: "#f9f9f9"}} 
+      customStyleContainerOrder = {{padding: "20px", backgroundColor: "#f9f9f9"}} data = {this.state.data} 
+      // tempPrice = {100000} 
+      // finalPrice= {300000} 
+      changeInputQuantity = {this.changeInputQuantity} 
+      // submitOrder = {this.submitOrder} 
+      text = {this.state.text}
+      />)
   }
 } 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import React from 'react'
 import styles from './styles.module.css' 
 import NumberFormat from "react-number-format";
 import NoneImage from 'noneImage.svg'
- 
+//  const text = {
+//   shopping_cart: "GIỎ HÀNG",
+//   provider: "Cung cấp bởi",
+//   delete: "Xoá",
+//   delete: "Xoá",
+//   buy_later: "Mua sau",
+//   temp_price: "Tạm tính",
+//   total_price: "Thành tiền",
+//   proceed_to_order: "Tiến hành đặt hàng",
+//   empty_cart: "Không có sản phẩm nào trong giỏ hàng của bạn",
+//   product: "Sản phẩm",
+// }
 export default class ECartComponent extends React.Component {
   constructor(props) {
     super(props);
@@ -10,12 +21,16 @@ export default class ECartComponent extends React.Component {
       quantity: 0,
       tempPrice: 0,
       finalPrice: 0,
-      data: []
+      data: [],
+      // text: this.props.text ||  
     }
   } 
   componentDidMount() {
     this.setState({data: this.props.data})
   }
+  // componentWillReceiveProps(nextProps) {
+     
+  // }
   changeQuantity = (id, newValue) => {
     if(this.changeInputQuantity) {
       // this.setState({quantity: newValue})
@@ -51,21 +66,25 @@ export default class ECartComponent extends React.Component {
     if(this.props.submitOrder) this.props.submitOrder(data)
   }
   formatNumber(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+    return x ? x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") : 0
   }
   actionClickOnTitle= (id) => { 
     if(this.props.actionClickOnTitle) this.props.actionClickOnTitle(id)
   }
   render() {
-  let {customButtomStyle,styleSubmitButton, styleAgreeButton, customStyleContainerCart, customStyleContainerOrder, fontFamily, backgroundColor, currencyUnit = "đ"} = this.props
+  let {sale_price, customButtomStyle,styleSubmitButton, styleAgreeButton, customStyleContainerCart, customStyleContainerOrder, fontFamily, backgroundColor, currencyUnit = "đ", tempPrice  , totalPrice , VAT = {hasVAT: true, valueVAT: 10} , text = {}} = this.props
   let {quantity, data} = this.state 
-  let tempPrice = 0
+  let tempPriceValue = tempPrice  || 0
+  if(!tempPriceValue) 
   data.forEach((e) => {
-    if(e.price) tempPrice += e.price*e.quantity
+    if(e.price) tempPriceValue += e.price*e.quantity
   })
+  let totalPriceValue = totalPrice ? totalPrice : tempPriceValue
+  if(VAT && VAT.hasVAT && VAT.valueVAT) totalPriceValue = totalPriceValue + totalPriceValue * VAT.valueVAT/100
+
   return  (
    <div style = {{backgroundColor: backgroundColor || "#f0f8ff00", fontFamily: fontFamily, height: "100%", width: "100%"}}> 
-    <div className = {styles.header}>GIỎ HÀNG <span className = {styles.headerSubTitle}>({data.length ? data.map((e) => e.quantity).reduce((a, b) => a + b) : 0} SẢN PHẨM)</span> </div>
+    <div className = {styles.header}>{text.shopping_cart || "CART"} <span className = {styles.headerSubTitle}>({data.length ? data.map((e) => e.quantity).reduce((a, b) => a + b) : 0} {text.product || "Products"})</span> </div>
     <div style = {customButtomStyle} className={styles.ecart_container}>
       <ul style = {customStyleContainerCart} className={styles.containerCart}>
         {data.length ? data.map((item, index) => {
@@ -75,14 +94,14 @@ export default class ECartComponent extends React.Component {
                 <div className={styles.ecart_productContent}>
                   <div  className={styles.infoProduct}>
                     <div className={styles.ecart_title} onClick = {() => this.actionClickOnTitle(item.id)}>{item.name}</div>
-                    <div className={styles.provider}>{`Cung cấp bởi: `}<a className={styles.providerName} href= "#">{item.provider}</a></div>
+                    <div className={styles.provider}>{`${text.provider || "Provider by"}: `}<a className={styles.providerName} href= "#">{item.provider}</a></div>
                     <div className={styles.action}>
-                      <a onClick = {() => this.deleteProduct(item.id)} href="#"  className={styles.actionDelete}>Xoá</a>
-                      <a onClick = {() => this.deleteProduct(item.id)} href="#" className={styles.actionBuyLater}>Mua sau</a>
+                      <a onClick = {() => this.deleteProduct(item.id)} href="#"  className={styles.actionDelete}>{text.delete || "Delete"}</a>
+                      <a onClick = {() => this.deleteProduct(item.id)} href="#" className={styles.actionBuyLater}>{text.buy_later || "Buy later"}</a>
                     </div>
                   </div>
                   <div  className={styles.infoRight}>
-                    <div className={styles.ecart_price}> {this.formatNumber(item.price)}{currencyUnit}</div>
+                      <div className={styles.ecart_price}> <div>{this.formatNumber(item.sale_price)}{currencyUnit}</div> <div><span className={styles.discountPrice}>{this.formatNumber(item.price)}{currencyUnit} </span><span className={styles.percentDiscount}>-{Math.ceil(item.price/item.sale_price) * 10}%</span></div></div>
                       <div className={styles.quanlityInput}> 
                         <button className={styles.quantity} onClick = {() => this.changeQuantity(item.id, item.quantity -1)}>-</button>
                         <input  className={styles.inputQuantity} value={item.quantity} onChange = {(event) => this.changeInputQuantity(item.id, event.target.value)}/>
@@ -93,15 +112,15 @@ export default class ECartComponent extends React.Component {
             </li>
           )
         })
-      : <div className = {styles.emptyCart}>Không có sản phẩm nào trong giỏ hàng của bạn</div>} 
+      : <div className = {styles.emptyCart}>{text.empty_cart || "Your cart is empty"}</div>} 
       </ul>
       <div style = {customStyleContainerOrder} className={styles.containerOrder}>
         <div  className={styles.cartPrice}>
-      <div className={styles.tempPrice}>Tạm tính:<span  className={styles.tempValue}>{this.formatNumber(tempPrice)}{currencyUnit}</span></div>
-      <div className={styles.tempPrice}>Thành tiền: <span className = {styles.priceValueFinal}>{this.formatNumber(tempPrice)}{currencyUnit}<i className = {styles.ecart_vat}>(Đã bao gồm VAT nếu có)</i></span></div>
+      <div className={styles.tempPrice}>{text.temp_price || "Temp price"}:<span  className={styles.tempValue}>{this.formatNumber(tempPriceValue)}{currencyUnit}</span></div>
+      <div className={styles.tempPrice}>{text.total_price || "Total price"}: <span className = {styles.priceValueFinal}>{this.formatNumber(totalPriceValue)}{currencyUnit}<i className = {styles.ecart_vat}>({text.includedVAT || "Included VAT"})</i></span></div>
         </div>
         <div >
-          <button style = {styleSubmitButton} onClick = {() => this.submitOrder(data)} className={styles.btnSubmit}>Tiến hành đặt hàng</button>
+      <button style = {styleSubmitButton} onClick = {() => this.submitOrder(data)} className={styles.btnSubmit}>{text.proceed_to_order || "Order"}</button>
         </div>
         <div  className={styles.cartPrice}>
           <div className={styles.tempPrice}>Mã giảm giá / Quà tặng</div>
